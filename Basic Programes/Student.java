579803b782f77ed014c323c7e2901db2a0954471
Siddhant soni <sidsoniii12@gmail.com>
2026-01-21 21:57:15 +0530

    Initial commit

---
+++ Basic Programes/Student.java
@@ -0,0 +1,19 @@
+class Student{
+         String name;
+		 int age;
+		 static String school = " ABC School ";
+		 
+		 void printName(){
+		 int a = 10;
+		 int b = 20;
+		 int c = a + b;
+		 System.out.println("Studentname is:" + name);
+		 }
+		 public static void main(String args [])
+		 {
+		 Student s1=new Student();
+		 System.out.println(s1.name="Siddhant");
+	}
+		 
+		 
+}
